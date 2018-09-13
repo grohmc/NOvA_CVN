db2ba37be33cad22520f445e48080e045770e4f6
Fernanda Psihas <psihas@fnal.gov>
2018-09-12 20:55:45 -0500

    Added simple function to plot plots...

--- all_plots.py
+++ all_plots.py
@@ -3,19 +3,26 @@ import matplotlib.pyplot as plt
 
 tables = h5Utils.importh5('fardet_genie_nonswap_genierw_fhc_v08_1000_r00014041_s60_c000_R17-11-14-prod4reco.h5')
 
+# Make plots from data tables
+def H1D(groupkey, varkey, data):
+    k1=groupkey
+    k2=varkey
+    plt.figure(1,(6,4))
+    plt.hist(data)
+    plt.title(k1+'.'+k2)
+    plt.savefig('all/'+k1+'.'+k2+'.png')
+    plt.close()
+    
 for k1 in tables.keys():
     df = tables[k1]
     for k2 in df.keys():
         if k2 == 'cvnmap':
             continue
+        if k2 != 'calE':
+            continue
 
         print(k1+'.'+k2)
         data = df[k2]
-        
-        data = data[(data==data) & (data < 1E10) & (data > -1E10)]
 
-        plt.figure(1,(6,4))
-        plt.hist(data)
-        plt.title(k1+'.'+k2)
-        plt.savefig('all/'+k1+'.'+k2+'.png')
-        plt.close()
+        data = data[(data==data) & (data < 1E10) & (data > -1E10)]
+        h5Utils.H1D(k1, k2, data)
