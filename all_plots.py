d431a99467320fdd7de375a327d4f8f05fb0e68c
Fernanda Psihas <psihas@fnal.gov>
2018-09-12 21:08:54 -0500

    Can color but can not label :(

--- all_plots.py
+++ all_plots.py
@@ -7,17 +7,21 @@ tables = h5Utils.importh5('fardet_genie_nonswap_genierw_fhc_v08_1000_r00014041_s
 def H1D(groupkey, varkey, data):
     k1=groupkey
     k2=varkey
-    plt.figure(1,(7,4))
-    plt.hist(data, color='hotpink',alpha=0.5)
-    plt.title(k1+'.'+k2)
-    plt.show()
-    #plt.savefig('all/'+k1+'.'+k2+'.png')
-    plt.close()
-    
+    h = plt
+    h.figure(1,(7,4))
+    h.hist(data, label='la', color='hotpink',alpha=0.5)
+    h.title(k1+'.'+k2)
+    h.show()
+    h.legend(loc='upper right')
+    #h.savefig('all/'+k1+'.'+k2+'.png')
+    h.close()
+
 for k1 in tables.keys():
     df = tables[k1]
     for k2 in df.keys():
         if k2 == 'cvnmap':
+            continue
+        if k1 != 'rec.slc':
             continue
         if k2 != 'calE':
             continue
