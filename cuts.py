92a5fa863030baf0b02a421e76629e6cdde2ee1e
grohmc <mcgroh@iu.edu>
2018-09-14 14:17:51 -0400

    Loosened numu cuts. Fixed muon cut.

--- cuts.py
+++ cuts.py
@@ -3,11 +3,11 @@ import numpy as np
 
 def kECut(tables):
     df = tables['rec.slc']
-    return (df.calE > 0.5) & (df.calE < 4)
+    return (df.calE > 0) & (df.calE < 5)
 
 def kHitsCut(tables):
     df = tables['rec.slc']
-    return (df.nhit > 50) & (df.nhit < 400)
+    return (df.nhit > 50) & (df.nhit < 600)
 
 def kCVNmCut(tables):
     df = tables['rec.sel.cvn2017']
@@ -15,25 +15,27 @@ def kCVNmCut(tables):
 
 def kContainCut(tables):
     df = tables['rec.sel.nuecosrej']
-    return (df.distallpngtop > 50) & \
-        (df.distallpngbottom > 20) & \
-        (df.distallpngfront > 20) & \
-        (df.distallpngback > 20) & \
-        (df.distallpngwest > 20) & \
-        (df.distallpngeast > 20)
+    return (df.distallpngtop > 20) & \
+        (df.distallpngbottom > 10) & \
+        (df.distallpngfront > 10) & \
+        (df.distallpngback > 10) & \
+        (df.distallpngwest > 10) & \
+        (df.distallpngeast > 10)
 
 def kPngCut(tables):
     df = tables['rec.vtx.elastic.fuzzyk']
-    df['clean'] = (df.npng < 4)
+    df['clean'] = (df.npng < 6)
     return df.clean.groupby(KL).agg(np.any)
 
 def kMuCut(tables):
-    df = tables['rec.vtx.elastic.fuzzyk.png.cvnpart']
-    df['ismuon'] = (df.muonid > 0.7)
-    return df.ismuon.groupby(KL).agg(np.any)
+    muid = tables['rec.vtx.elastic.fuzzyk.png.cvnpart'].muonid
+    len = tables['rec.vtx.elastic.fuzzyk.png'].len
+    df = (muid > 0.5) | (len > 5)
+    return df.groupby(KL).agg(np.any)
 
 def kFullCut(tables):
-    return kECut(tables) & kHitsCut(tables) & kCVNmCut(tables) & kContainCut(tables) & kPngCut(tables) & kMuCut(tables)
+    return kECut(tables) & kHitsCut(tables) & kCVNmCut(tables) & \
+        kContainCut(tables) & kPngCut(tables) & kMuCut(tables)
 
 def kCalEVar(tables):
     df = tables['rec.slc']
@@ -45,7 +47,7 @@ def kNuPdg(tables):
 
 def kNuInt(tables):
     df = tables['rec.mc.nu']
-    return tables['mode'] # mode is panda internal function. Awkward...
+    return df['mode'] # mode is panda internal function. Awkward...
 
 ######################################################################################
 
