516f38b3fb7839531fff6d40b00fb602a3ac03b4
grohmc <mcgroh@iu.edu>
2018-09-15 03:35:34 -0400

    Added integral. The cuts still don't really work the way I want.

--- spectrum.py
+++ spectrum.py
@@ -6,17 +6,17 @@ class spectrum():
         self._tables = tables
         self._var = var(tables)
         self._cut = np.all(pd.concat([c(tables) for c in cut], axis=1), axis=1)
-        
+
     #def fill(self): I can't think of a reason to do it this way...
         self._df = self._var[self._cut]
         self._POT = self._tables['spill'].spillpot.sum()
-        
+
     def POT(self):
         return self._POT
-    
+
     def df(self):
         return self._df
-    
+
     def histogram(self, bins=None, range=None, POT=None):
         if not POT:
             POT = self._POT
@@ -24,8 +24,13 @@ class spectrum():
             n, bins = np.histogram(self._df, bins, range)
         else:
             n, bins = np.histogram(self._df)
-            
+
         return n*POT/self._POT, bins
-    
+
     def entries(self):
         return self._df.shape[0]
+
+    def integral(self,POT=None):
+        if not POT:
+            POT = self._POT
+        return self.entries()*POT/self._POT
