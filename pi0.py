43fc842dd3c14a2a170e81d1dbd1c2f148167a40
grohmc <mcgroh@iu.edu>
2018-09-15 13:40:27 -0400

    Updated a bit. I'd like to do this using spectra...

--- pi0.py
+++ pi0.py
@@ -1,6 +1,9 @@
 import h5Utils
+from plotUtils import integral
+
 import numpy as np
 import pandas as pd
+
 import matplotlib.pyplot as plt
 from matplotlib import rc
 rc('text', usetex=True)
@@ -59,46 +62,35 @@ def kMass(tables):
     # return a dataframe with a single column of the invariant mass
     return 1000*deadscale*np.sqrt(2*EProd*(1-dot))
 
-# compute the integral given hist contents and bin boundaries
-def integral(n, bins, range=None, width=False):
-    if not range:
-        range=(0,len(bins))
-    if width:
-        return (np.diff(bins[slice(*range)])*n[slice(*range)]).sum()
-    else:
-        return n[slice(*range)].sum()
-
 tables = h5Utils.importh5('neardet_genie_nonswap_genierw_fhc_v08_1535_r00010921_s02_c002_R17-11-14-prod4reco.d_v1_20170322_204739_sim.repid.root.hdf5')
 
 cut = twoprong(tables) & kGammaCut(tables) & kContain(tables)
-cutSig = cut & kTruePi0(tables)
 cutBkg = cut & ~kTruePi0(tables)
 
-data = kMass(tables)[cut]
+dfData = kMass(tables)[cut]
 dfTot = kMass(tables)[cut]
 dfBkg = kMass(tables)[cutBkg]
 
 plt.figure(1,figsize=(6,4))
 
 # could use np.histogram to get these values, but we are plotting anyway so grab them here
-totn,bins,_ = plt.hist(dfTot, 10, (0,300), color='xkcd:red', label='$\pi^0$ Signal', histtype='step')
-bkgn,_,_ = plt.hist(dfBkg, 10, (0,300), color='xkcd:dark blue', label='Background')
+tot,bins,_ = plt.hist(dfTot, 10, (0,300), color='xkcd:red', label='$\pi^0$ Signal', histtype='step')
+bkg,_,_    = plt.hist(dfBkg, bins, color='xkcd:dark blue', label='Background')
 
-plt.xlabel('M$_{\gamma\gamma}$ (MeV)')
+plt.xlabel('M$_{\gamma\gamma}$')
 plt.ylabel('Events')
 
-# we need to subtract the bkg
-intBkg = integral(bkgn, bins)
-intSig = integral(totn-bkgn, bins)
-
-pur = intSig / (intBkg + intSig)
-print('This selection has a pi0 purity of ' + str(pur))
-
 # There must be a more direct way to do this.
-d,_ = np.histogram(data,bins)
+d,_ = np.histogram(dfData,bins)
 centers = (bins[:-1] + bins[1:])/2
 plt.errorbar(centers,d, yerr=np.sqrt(d), fmt='ko', label='Fake Data')
 
 plt.legend(loc='upper right')
+
+inttot = integral(tot, bins)
+intbkg = integral(bkg, bins)
+
+pur = (inttot-intbkg) / inttot
+print('This selection has a pi0 purity of ' + str(pur))
 
 plt.show()
