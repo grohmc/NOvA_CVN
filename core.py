3873c59bb273dee1692d2f2cf6f563203d714e4f
grohmc <mcgroh@iu.edu>
2018-09-15 16:43:04 -0400

    updated for cut structure.

--- core.py
+++ core.py
@@ -6,7 +6,7 @@ class spectrum():
         #self._POT = 0
         self._tables = tables
         self._var = var(tables)
-        self._cut = np.all(pd.concat([c(tables) for c in cut], axis=1), axis=1)
+        self._cut = cut.cut(tables)
 
     #def fill(self): I can't think of a reason to do it this way...
         self._df = self._var[self._cut]
@@ -19,8 +19,7 @@ class spectrum():
         return self._df
 
     def histogram(self, bins=None, range=None, POT=None):
-        if not POT:
-            POT = self._POT
+        if not POT: POT = self._POT
         if bins:
             n, bins = np.histogram(self._df, bins, range)
         else:
@@ -32,8 +31,7 @@ class spectrum():
         return self._df.shape[0]
 
     def integral(self,POT=None):
-        if not POT:
-            POT = self._POT
+        if not POT: POT = self._POT
         return self.entries()*POT/self._POT
 
 class cut():
